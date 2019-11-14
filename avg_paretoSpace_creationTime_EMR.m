ae438e179b13e33965c1d0029ba7264781828899
akbacak <33653031+akbacak@users.noreply.github.com>
2019-11-15 00:33:41 +0300

    Add files via upload

--- avg_paretoSpace_creationTime_EMR.m
+++ avg_paretoSpace_creationTime_EMR.m
@@ -3,11 +3,11 @@ close all;
 clc;
 
 load('lamdaDataset/hashCodes/filenames.mat');
-load('lamdaDataset/hashCodes/hashCodes_512.mat');
+load('lamdaDataset/hashCodes/hashCodes_256.mat');
 load('lamdaDataset/hashCodes/targets.mat');
 
     N = 2000;   % Number of samples in the Lamda Dataset
-    data = hashCodes_512; % Binary features (Hash codes) N x NumberHasBits
+    data = hashCodes_256; % Binary features (Hash codes) N x NumberHasBits
     
     queryIndex = xlsread('qLabels_V2.xls');  % Reads randomly choosen query pairs from excell file
     queryIndex = transpose( queryIndex ); 
@@ -15,22 +15,25 @@ load('lamdaDataset/hashCodes/targets.mat');
     queryIndex2 = queryIndex(2,:); % Second element of Qury Pair
     
   tic  
-  for l = 1:500  % Number of Query Pairs
+   for l = 1:500  % Number of Query Pairs
         
+        queryIndex1(l) = queryIndex(1,l);
+        queryIndex2(l) = queryIndex(2,l);
         q1 = data(queryIndex1,:);  % q1 & q2 are query pairs in the loop
         q2 = data(queryIndex2,:);   
                  
-        [H A landmarks Z] = EMRcomputeModel(data); % Compute EMR
-        y1 = zeros(N,1);
-        y1(queryIndex1) = 1; % Ranking  of query1 = 1
-        y2 = zeros(N,1);
-        y2(queryIndex2) = 1; % Ranking  of query2 = 1
-        
-        simEMR1{l,:} = EMRscore(H ,A, y1);  % EMR Ranking
-        simEMR2{l,:} = EMRscore(H ,A, y2);  % EMR Ranking
+     
+        y1{l,:} = zeros(N,1);
+        y1{l,:}(queryIndex1(l)) = 1; % Ranking  of query1 = 1
+        y2{l,:} = zeros(N,1);
+        y2{l,:}(queryIndex2(l)) = 1; % Ranking  of query2 = 1
+    
+    
+     [H A landmarks Z] = EMRcomputeModel(data); % Compute EMR
+        simEMR1{l,:} = EMRscore(H ,A, y1{l,:});  % EMR Ranking
+        simEMR2{l,:} = EMRscore(H ,A, y2{l,:});  % EMR Ranking
         dist1{l,:}   = 1-simEMR1{l,:};      % Dissimilarity 
         dist2{l,:}   = 1-simEMR2{l,:};      % Dissimilarity 
-            
         
         
         X = zeros(2,N);
