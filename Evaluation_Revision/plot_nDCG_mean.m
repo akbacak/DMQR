ca53873390d75770ad8291ffaacb7848ea2c874b
akbacak <33653031+akbacak@users.noreply.github.com>
2020-04-28 03:16:36 +0300

    Add files via upload

--- Evaluation_Revision/plot_nDCG_mean.m
+++ Evaluation_Revision/plot_nDCG_mean.m
@@ -4,28 +4,28 @@ clc;
 x=linspace(0,350,1);
 
 
-load('Evaluation_Revision/streetsDataset/DMQR/streets_2d_16_DMQR_n_DCG_mean');
-plot(n_DCG_mean, '--g', 'LineWidth',3); 
+load('Evaluation_Revision/lamdaDataset/DMQR/5d_16_DMQR_n_DCG_mean');
+plot(n_DCG_mean, '-.k', 'LineWidth',3); 
 hold on
 
-load('Evaluation_Revision/streetsDataset/DMQR/streets_2d_32_DMQR_n_DCG_mean');
-plot(n_DCG_mean,'-k', 'MarkerSize',18, 'LineWidth',2); 
+load('Evaluation_Revision/lamdaDataset/DMQR/5d_32_DMQR_n_DCG_mean');
+plot(n_DCG_mean,'-r', 'MarkerSize',18, 'LineWidth',4); 
 hold on
 
-load('Evaluation_Revision/streetsDataset/DMQR/streets_2d_64_DMQR_n_DCG_mean');
-plot(n_DCG_mean,'-.b', 'MarkerSize',18, 'MarkerFaceColor',[0 0 1], 'LineWidth',3);
+load('Evaluation_Revision/lamdaDataset/DMQR/5d_64_DMQR_n_DCG_mean');
+plot(n_DCG_mean,'-b', 'MarkerSize',8, 'MarkerFaceColor',[0 0 1], 'LineWidth',3);
 hold on
 
-load('Evaluation_Revision/streetsDataset/DMQR/streets_2d_128_DMQR_n_DCG_mean');
-plot(n_DCG_mean,'-+r',  'MarkerSize',16, 'LineWidth',3');
+load('Evaluation_Revision/lamdaDataset/DMQR/5d_128_DMQR_n_DCG_mean');
+plot(n_DCG_mean,'-gd',  'MarkerSize',10, 'MarkerFaceColor',[0 1 0], 'LineWidth',1');
 hold on
 
-load('Evaluation_Revision/streetsDataset/DMQR/streets_2d_256_DMQR_n_DCG_mean');
-plot(n_DCG_mean,'--b', 'MarkerSize',18, 'MarkerFaceColor',[0 0 1], 'LineWidth',3);
+load('Evaluation_Revision/lamdaDataset/DMQR/5d_256_DMQR_n_DCG_mean');
+plot(n_DCG_mean,'-bo', 'MarkerSize',10, 'MarkerFaceColor',[0 0 1], 'LineWidth',1);
 hold on
 
-load('Evaluation_Revision/streetsDataset/DMQR/streets_2d_512_DMQR_n_DCG_mean');
-plot(n_DCG_mean,'sr',  'MarkerSize',6, 'LineWidth',3');
+load('Evaluation_Revision/lamdaDataset/DMQR/5d_512_DMQR_n_DCG_mean');
+plot(n_DCG_mean,'-rs',  'MarkerSize',10, 'MarkerFaceColor',[1 0 0],  'LineWidth',1');
 hold on
 
 
